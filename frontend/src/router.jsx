dc9dd234f3aa3eac8890d16738d5597556e5b5e7
Nabin Singh <nabin@skilluplabs.com.au>
2024-05-13 19:32:45 +1000

    Added router to front-end

---
+++ frontend/src/router.jsx
@@ -0,0 +1,13 @@
+import {
+    createBrowserRouter
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div className="text-center text-8xl m-10">E-commerce</div>,
+    },
+]);
+
+export default router;
