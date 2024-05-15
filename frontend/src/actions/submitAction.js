e50f05469e7b1fcddbfc7a73cdb183e79858e065
Nabin Singh <nabin@skilluplabs.com.au>
2024-05-15 17:59:59 +1000

    Add backend service

--- frontend/src/actions/submitAction.js
+++ frontend/src/actions/submitAction.js
@@ -4,6 +4,9 @@ async function submitAction(data) {
     const url = `${API_URL}/api/v1/auth/register`;
     const response = await fetch(url, {
         method: 'post',
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: JSON.stringify(data)
     });
     return response;
